34459cefb4f02ccb1e71bce041f418abf8039bee
Sebastianwyh <32828615+Sebastianwyh@users.noreply.github.com>
2018-01-22 14:48:41 +0100

    Update Movie_Website.py

--- Movie_Website.py
+++ Movie_Website.py
@@ -8,35 +8,58 @@ This file is to edit the info of videos.
 import media
 import fresh_tomatoes
 
-
+"""
+The Arrow TV show: TV show title, storyline, season, 
+poster image and youtube trail url
+"""
 The_Arrow = media.TV_show(
     "The Arrow", "A story of a DC super hero -- Oliver Queen",
     "Season 1",
     "http://img8.dm530.net/pic/uploadimg/2015-4/5092.jpg",
     "https://www.youtube.com/watch?v=ofzPONG8hDU")
 
+"""
+Nijita TV show: TV show title, storyline, season, 
+poster image and youtube trail url
+"""
 Nikita = media.TV_show(
     "Nikita", "A girl named Nikita embarked on the raod to killer.",
     "Season 1",
     "http://stuffpoint.com/nikita/image/85100-nikita-nikita-poster.jpg",
     "https://www.youtube.com/watch?v=IhelGgaa2Wg")
 
+"""
+Scorpion TV show: TV show title, storyline, season, 
+poster image and youtube trail url
+"""
 Scorpion = media.TV_show(
     "Scorpion", "The high IQ team helps DHS with defending attack.",
     "Season 1",
     "https://s-media-cache-ak0.pinimg.com/originals/96/23/32/96233256645e445e7ab6c2775d80cba5.jpg",  # NOQA
     "https://www.youtube.com/watch?v=l4QFpheS_JQ")
 
+"""
+Sky High: movie title, storyline, 
+poster image and youtube trail url
+"""
 Sky_High = media.Movie(
     "Sky High", "A story about Superman's son",
     "https://upload.wikimedia.org/wikipedia/en/2/29/Sky_High_movie_poster.jpg",
     "https://www.youtube.com/watch?v=G7aMWVN1ThM")
 
+"""
+El bar: movie title, storyline, 
+poster image and youtube trail url
+"""
 El_bar = media.Movie(
     "El bar", "Who went our, who got killed",
     "http://cinehorizontes.com/wp-content/uploads/el-bar-alex-de-la-iglesia-508x320.jpg",  # NOQA
     "https://www.youtube.com/watch?v=PTazqyeYg3Y")
 
+"""
+She is the man: movie title, storyline, 
+poster image and youtube trail url
+"""
 She_is_the_Man = media.Movie(
     "She is the man", "Who says a girl cannot play football better than boys?",
     "https://images-na.ssl-images-amazon.com/images/I/51Tp79i4HoL._SY445_.jpg",  # NOQA
